1c34301ed26cd4a67e55a09c164b1010db4fcaf6
Antonio Ramos de Carvalho Júnior <antonio@meliuz.com.br>
2018-06-02 08:10:50 -0400

    Add the SearchViewController to the project

---
+++ marvel-superhero/marvel-superhero/View Controllers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  marvel-superhero
+//
+//  Created by Antonio de Carvalho Jr on 02/06/18.
+//  Copyright © 2018 Antonio R de Carvalho Jr. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
